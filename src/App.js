c08ac12889c7097ccc7fa8cd04158db669727be4
FikruDemlew <fikrudemlew@gmail.com>
2023-10-27 15:23:54 +0300

    update our menu-item to navigate withRouter HOC

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './App.css';
+
+import { Route, Switch } from 'react-router-dom'
 import HomePage from './pages/homepage/homepage.component';
+
+
+const HatePage = () => {
+  return <h1>Hats Page</h1>
+}
 function App() {
   return (
     <div>
-   <HomePage />
+      
+       <Route exact path='/' component={HomePage}/>
+       <Route exact path='/hats' component={HatePage}/>
+       
     </div>
   );
 }
